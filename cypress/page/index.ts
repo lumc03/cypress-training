85d2cbd797af549884d087931bbe423f287415f2
lumc03 <107214750+lumc03@users.noreply.github.com>
2022-08-04 07:45:27 -0500

    AAA pattern (#10)

--- cypress/page/index.ts
+++ cypress/page/index.ts
@@ -2,8 +2,7 @@ export { MenuContentPage } from "./menu-content.page";
 export { ProductListPage } from "./products-list.page";
 export { ShopingCartPage } from "./shoping-cart.page";
 export { LoginPage } from "./login.page";
-export { AdressPage }  from "./address-step.page";
+export { AdressPage } from "./address-step.page";
 export { ShippingPage } from "./shipping-step.page";
 export { PaymentPage } from "./payment-step.page";
-
-
+export { DressesListPage } from "./dresses-list.page";
